2811e5d34ca3152470cabaa95758b2e6baf6850c
Jason Chen <jasonchen6699@gmail.com>
2017-02-05 19:04:17 -0500

    Revert "This is a prank"
    This reverts commit b75825c8427003c397404f7081ffb58dababd49a because
    it was used to demonstrate git.

--- jason_and_caroline/bump_sensor.py
+++ jason_and_caroline/bump_sensor.py
@@ -26,7 +26,6 @@ class SquareBot():
             if self.bump_sensor.is_on():
                 self.back_and_turn()
 
-            print("I was here!")
             sys.sleep(1)
 
     def back_and_turn(self):
