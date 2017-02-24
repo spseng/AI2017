bbba8d655df4aa01df90a00eabe29178b6ed552c
Jason Chen <jasonchen6699@gmail.com>
2017-02-24 12:57:44 -0500

    Receive messages from the vex to print on LCD

--- jason_and_caroline/arduino/serial_vex.ino/serial_vex.ino.ino
+++ jason_and_caroline/arduino/serial_vex.ino/serial_vex.ino.ino
@@ -2,6 +2,8 @@
 
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 String message;
+bool started = false;
+int num_count = 0;
 
 void setup() {
   // put your setup code here, to run once:
@@ -10,10 +12,25 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  while (Serial.available() > 0) {
-    message = Serial.readString();
-    Serial.println(message);
-    lcd.clear();
-    lcd.print(message);
+  if (!started) {
+    if (Serial.available() > 0) {
+      if (Serial.read() == 232) {
+        started = true;
+        Serial.flush();
+      }
+    }
+  }
+  else {
+    if (Serial.available() > 0) {
+      message = Serial.readStringUntil('\0');
+      Serial.flush();
+      message.trim();
+      if (!(message.length() < 1) && num_count > 0) {
+        Serial.println(message);
+        lcd.clear();
+        lcd.print(message);
+      }
+      num_count++;
+    }
   }
 }
