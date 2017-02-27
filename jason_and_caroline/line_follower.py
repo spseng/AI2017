030041bb143c5966598d40b6e5aa4b67bcb89a01
Jason Chen <jasonchen6699@gmail.com>
2017-02-27 09:23:24 -0500

    Removed whitespaces

--- jason_and_caroline/line_follower.py
+++ jason_and_caroline/line_follower.py
@@ -5,9 +5,9 @@ import vex
 class sensor_bot():
     #globals
     TOTAL_POWER = 70
-    
+
     #configurations
-    
+
     def __init__(self):
         self.left_wheels = vex.Motor(10)
         self.right_wheels = vex.Motor(1)
@@ -17,25 +17,25 @@ class sensor_bot():
         self.serial = vex.Serial(1, 9600)
         self.serial.write("begin")
         sys.sleep(2)
-    
+
     def turn_left(self):
         self.left_wheels.run(self.TOTAL_POWER)
         self.right_wheels.run(-self.TOTAL_POWER)
-        
+
     def turn_right(self):
         self.left_wheels.run(-self.TOTAL_POWER)
         self.right_wheels.run(self.TOTAL_POWER)
-        
+
     def drive_forward(self):
         self.left_wheels.run(self.TOTAL_POWER)
         self.right_wheels.run(self.TOTAL_POWER)
-        
+
     def detect(self):
         self.serial.write("left")
         sys.sleep(0.5)
         self.serial.write(str(self.left_line_tracker.line_tracker_percent()))
         sys.sleep(1)
-        
+
         self.serial.write("right")
         sys.sleep(0.5)
         self.serial.write(str(self.right_line_tracker.line_tracker_percent()))
